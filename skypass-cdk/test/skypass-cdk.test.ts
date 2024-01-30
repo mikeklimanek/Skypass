7a25c69914428b8373f22ebbcf155e0bb3aaadd1
mikeklimanek <mike.klimanek@gmail.com>
2024-01-31 00:21:47 +0100

    sync AWS CDK CLI

---
+++ skypass-cdk/test/skypass-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SkypassCdk from '../lib/skypass-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/skypass-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SkypassCdk.SkypassCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
